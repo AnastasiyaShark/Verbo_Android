e9ff3f574e942be90a0fcd1ae7cde162319a52d5
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-08-11 09:52:42 +0200

    - add login screen

---
+++ app/src/main/java/com/ashark/verbo/LoginScreenActivity.java
@@ -0,0 +1,14 @@
+package com.ashark.verbo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+}
