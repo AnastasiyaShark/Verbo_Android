f5826eabfecf1c880d832f3c6d4feb1a02747951
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-08-11 18:00:30 +0200

    - fix MainActivity.java

--- app/src/main/java/com/ashark/verbo/MainActivity.java
+++ app/src/main/java/com/ashark/verbo/MainActivity.java
@@ -3,12 +3,36 @@ package com.ashark.verbo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login,signUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+        signUp = findViewById(R.id.signUp);
+        //внутри что будет происходить при нажатии на кнопку
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        //внутри что будет происходить при нажатии на кнопку
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
