57da07753491541f39fa365cf848df8568652525
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-08-11 16:53:15 +0200

    - add signup screen

---
+++ app/src/main/java/com/ashark/verbo/SignupScreenActivity.java
@@ -0,0 +1,14 @@
+package com.ashark.verbo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_screen);
+    }
+}
