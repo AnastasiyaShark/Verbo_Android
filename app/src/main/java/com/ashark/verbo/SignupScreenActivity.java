f7a0f673cf2a3ced82325c1e0b95f1ba0d56b6e7
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-08-12 15:46:25 +0200

    - fix SignupScreenActivity.java

--- app/src/main/java/com/ashark/verbo/SignupScreenActivity.java
+++ app/src/main/java/com/ashark/verbo/SignupScreenActivity.java
@@ -1,8 +1,13 @@
 package com.ashark.verbo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
 
@@ -20,7 +25,56 @@ public class SignupScreenActivity extends AppCompatActivity {
         email = findViewById (R.id.signup_email);
         nativeLanguage = findViewById (R.id.signup_native_language);
         learningLanguage = findViewById (R.id.signup_learning_language);
+
+        nativeLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInfoAboutLang();
+
+            }
+        });
     }
+    String[] languages ={"Swedish","English","Russian"};
+    boolean [] checkedSites = new boolean[]{false,false,false};
+
+    public void showInfoAboutLang() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Native language");
+        dialog.setMessage("Choose your native language");
+
+        //Building the list to be shown in AlertDialog
+       dialog.setMultiChoiceItems(languages,checkedSites, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                // Update the current item's checked status
+                checkedSites[which] = isChecked;
+            }
+        });
+        //Set Positive Button
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"OK button was pressed",Toast.LENGTH_LONG).show();
+
+            }
+        });
+        //Set Negative Button
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"Cancel button was pressed",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //Creating AlertDialog
+        AlertDialog alterDialog = dialog.create();
+        //Displaying AlertDialog
+        alterDialog.show();
+
+
+
+    }
+
 
 
 }
