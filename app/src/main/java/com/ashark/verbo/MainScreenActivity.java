0ea53516d6fc66c77a71b434932ca974c77f3468
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-08-14 15:39:22 +0200

    - add UserScreenActivity.java - add MainScreenActivity.java

--- app/src/main/java/com/ashark/verbo/MainScreenActivity.java
+++ app/src/main/java/com/ashark/verbo/MainScreenActivity.java
@@ -2,13 +2,30 @@ package com.ashark.verbo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainScreenActivity extends AppCompatActivity {
+
+    Button learnVerbs, repeatVerbs, exam, grammar, profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        learnVerbs = findViewById(R.id.learn_verbs);
+        repeatVerbs = findViewById(R.id.repeat_verbs);
+        exam = findViewById(R.id.exam);
+        grammar = findViewById(R.id.grammar);
+        profile = findViewById(R.id.profile);
+
+        profile.setOnClickListener(v -> {
+            Intent intent = new Intent(MainScreenActivity.this, UserScreenActivity.class);
+            startActivity(intent);
+
+        });
+
     }
 }
--- app/src/main/java/com/ashark/verbo/UserScreenActivity.java
+++ app/src/main/java/com/ashark/verbo/UserScreenActivity.java
@@ -1,14 +1,54 @@
 package com.ashark.verbo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class UserScreenActivity extends AppCompatActivity {
+
+    ImageButton back;
+    Button account, vocabulary, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_screen);
+
+        back = findViewById(R.id.back);
+        account = findViewById(R.id.account);
+        vocabulary = findViewById(R.id.vocabulary);
+        logout = findViewById(R.id.logout);
+
+        back.setOnClickListener(v -> {
+            Intent intent = new Intent(UserScreenActivity.this,MainScreenActivity.class);
+            startActivity(intent);
+        });
+
+        logout.setOnClickListener(v -> {
+            openQuitDialog();
+        });
+
+    }
+
+    private void openQuitDialog() {
+        AlertDialog.Builder quitDialog = new AlertDialog.Builder(
+                UserScreenActivity.this);
+        quitDialog.setTitle("Exit");
+        quitDialog.setMessage("Are you sure?");
+
+        quitDialog.setPositiveButton("Logout", (dialog, which) -> {
+//                finish();
+            Intent intent = new Intent(UserScreenActivity.this,MainActivity.class);
+            startActivity(intent);
+        });
+
+        quitDialog.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
+
+        quitDialog.show();
     }
 }
