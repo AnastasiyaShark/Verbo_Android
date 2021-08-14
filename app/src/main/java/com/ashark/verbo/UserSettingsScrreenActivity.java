ef3ce0b2cb2be025689aeaf65773b84a38bfc4de
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-08-14 16:04:47 +0200

    - add user_settings_scrreen.xml

---
+++ app/src/main/java/com/ashark/verbo/UserSettingsScrreenActivity.java
@@ -0,0 +1,14 @@
+package com.ashark.verbo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserSettingsScrreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_settings_scrreen);
+    }
+}
