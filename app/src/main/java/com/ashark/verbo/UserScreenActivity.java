de52cdf0390e798ccb24403b183daf72a5c1a8cc
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-08-14 14:00:31 +0200

    - add icons

---
+++ app/src/main/java/com/ashark/verbo/UserScreenActivity.java
@@ -0,0 +1,14 @@
+package com.ashark.verbo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_screen);
+    }
+}
